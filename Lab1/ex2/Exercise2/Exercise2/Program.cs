74f561aa20401742d33b371169fe73863f7b3294
Artsiom Kukankou <147983587+ypayel@users.noreply.github.com>
2025-10-16 11:43:47 +0200

    new exesices

---
+++ Lab1/ex2/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    /// <summary>
+    /// Exercise 2 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Uses ordered generic dynamic array
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            OrderedDynamicArray<int> tablica = new OrderedDynamicArray<int>();
+            tablica.Add(3);
+            tablica.Add(1);
+            tablica.Add(2);
+            Console.WriteLine(tablica);
+
+            Prostokat prostokat1 = new Prostokat(4, 3);
+            Prostokat prostokat2 = new Prostokat(5, 2);
+            Prostokat prostokat3 = new Prostokat(1, 11);
+
+            OrderedDynamicArray<Prostokat> tab2 = new OrderedDynamicArray<Prostokat>();
+
+            tab2.Add(prostokat1);
+            tab2.Add(prostokat2);
+            tab2.Add(prostokat3);
+
+            Console.WriteLine(tab2);
+            
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ Lab1/ex2/Exercise2/Exercise2/Prostokat.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    internal class Prostokat : IComparable
+    {
+        private int width;
+        private int height;
+        private int area;
+
+        public Prostokat(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.area = this.height * this.width;
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Prostokat otherProstokat = obj as Prostokat;
+            if (otherProstokat != null)
+                return this.area.CompareTo(otherProstokat.area);
+            else
+                throw new ArgumentException("Object is not a Prostokat");
+        }
+
+            public override string ToString()
+        {
+            return "Prostokat: " + width + "*" + height;
+        }
+
+    }
+
+        
+    }
+
